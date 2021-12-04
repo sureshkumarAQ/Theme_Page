4b6ed5427524b3d497122b19825ee3ddf51ff802
Suresh Kumar <sureshkumar23102000@gmail.com>
2021-12-04 15:58:25 +0530

    file added

---
+++ vscript.js
@@ -0,0 +1,13 @@
+VANTA.HALO({
+  el: "#theme-page",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  backgroundColor: 0xa031b,
+  amplitudeFactor: 1.7,
+  xOffset: -0.01,
+  yOffset: -0.04,
+  size: 1,
+});
